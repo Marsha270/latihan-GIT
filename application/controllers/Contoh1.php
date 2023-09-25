b54696f18cd2db47d491289e936db95ad4783638
Marsha270 <Marshaangelica82@gmail.com>
2023-09-25 16:12:39 +0700

    menambahkan pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Marsha Angelica Avrilia <br>
+            Nim saya 19220327 <br>
+            saya dari kelas 19.3A.26 <br>
+            Saya tinggal di Rumah <br>
+            saya menyukai musik <br>";
+    }
+}
